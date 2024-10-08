a646f7c62e40bc6dad13193a4db758ee28648d7c
Mahmut Can Özgan <mahmutcan.ozgan@onedose.io>
2024-10-08 19:02:29 +0300

    deploy hataları

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,31 +1,14 @@
 import { getBaseUrl } from '@/utils/Helpers';
 import type { MetadataRoute } from 'next';
-import { getSitemap } from '@/utils/getSitemap'; // Use the dynamic sitemap generator
-import { useTranslations } from 'next-intl';
 
-// Generate the sitemap using the dynamically loaded sitemap entries
+
 export default function sitemap(): MetadataRoute.Sitemap {
-    const t = useTranslations('Sitemap');
-
-    const baseUrl = getBaseUrl();
-    const sitemapEntries = getSitemap(t); // Retrieves all pages from your sitemap utility
-
-    // Construct the sitemap entries dynamically
-    const sitemapRoutes = sitemapEntries.map(entry => ({
-        url: `${baseUrl}${entry.link}`, // Dynamic URLs based on sitemap
-        lastModified: new Date(),
-        changeFrequency: 'daily' as 'daily',  // Correct typing for changeFrequency
-        priority: 0.7,  // Default priority, can be adjusted per page
-    }));
-
-    // Return the home page with higher priority
     return [
         {
-            url: `${baseUrl}/`,
+            url: `${getBaseUrl()}/`,
             lastModified: new Date(),
-            changeFrequency: 'daily' as 'daily', // Correct typing for changeFrequency
-            priority: 1.0,  // Home page gets a higher priority
+            changeFrequency: 'daily',
+            priority: 0.7,
         },
-        ...sitemapRoutes,  // Spread all other routes
     ];
 }
