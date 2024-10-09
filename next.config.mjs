af4d104c64d8d12c123e55fab87875a86360271b
Mahmut Can Özgan <mahmutcan.ozgan@onedose.io>
2024-10-09 22:33:29 +0300

    Türkçe İngilizce Dil ayarları yapıldı 3

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,8 @@
 import createNextIntlPlugin from 'next-intl/plugin'
-import path from 'path'
 
 const withNextIntl = createNextIntlPlugin()
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
-    // 'attr-accept' modülü için alias ekleniyor
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      'attr-accept': path.resolve(__dirname, 'node_modules/attr-accept'),
-    };
-    return config;
-  },
-};
+const nextConfig = {}
 
 export default withNextIntl(nextConfig)
